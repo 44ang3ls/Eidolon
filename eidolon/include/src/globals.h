e7b17c522a90f9f7cb77b5388d3a7fea45988db3
44ang3ls <82519853+44ang3ls@users.noreply.github.com>
2025-01-09 10:57:10 -0500

    tweak

--- eidolon/include/src/globals.h
+++ eidolon/include/src/globals.h
@@ -7,7 +7,7 @@
 #define GLOBALS_H
 
 // current version_id
-const char* version_id = "Eidolon Prototype 0.0.2a";
+const char* version_id = "Eidolon Prototype 0.1.0";
 
 // currently selected color
 static float current_color[4] = { 0.0f, 0.0f, 0.0f, 1.0f };
